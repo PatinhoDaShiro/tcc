4f5a1b46f3ab41fb70d0f04b9897eb4f20cf2dbb
PatinhoDaShiro <81689830+PatinhoDaShiro@users.noreply.github.com>
2022-06-01 17:38:52 -0300

    Add files via upload

---
+++ TGE/home.php
@@ -0,0 +1,34 @@
+<?php
+include 'config.php';
+include TEMPLATE_BASE.'/header.php';
+include TEMPLATE_BASE.'/nav.php';
+?>
+<body>
+    <section>
+
+    <div id="welcome">
+    <h1>Seja bem vindo à nossa comunidade!</h1>
+    </div>
+    
+    <div id="div_registro">
+        <button id="registrar">
+            <h2>Junte-se a nós</h2>
+        </button>
+    </div>
+    <p>The Gamers Encounter promete reunir seus usuarios para uma otima jogatina com vários amigos, por que não se juntar a nossa comunidade gamer?</p>
+    </section>
+    
+        <div id="jogos">
+        <div id="carousel">
+        <div class="imagem"><img  class="img" src="<?php echo URL_BASE?>/imagens/image.png " alt=""></div>
+        <div class="imagem"><img  class="img" src="<?php echo URL_BASE?>/imagens/imag2.png " alt=""></div>
+      
+
+        
+        </div>
+        </div>
+      
+        
+    
+    
+</body>
---
+++ TGE/templates/nav.php
@@ -0,0 +1,25 @@
+ <nav  id="logo" >
+      <div  id="nav">
+        <a >
+          <img src="<?php echo URL_BASE?>/imagens/logo.png" alt="" width="116" height="70" >
+          
+          <div id="perfil_link">
+          <img  src="<?php echo URL_BASE?>/imagens/icon.png" alt="" width="40" height="40" id="icon" >
+          <span id="nick">Iniciar sessão</span>
+          
+          </div>
+         
+        </a>
+      </div>
+    </nav>
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
